cd2e44be39c9f3927d51903fdb3bc895ed7a09e3
陆张弛 <li_y_c@qq.com>
2026-02-11 19:07:37 +0800

    test(benchmark): 添加 log10 和 log10_ 到一元逐点性能测试
    为了更全面地评估一元逐点操作的性能，将 log10 和 log10_ 函数加入基准测试列表。

--- benchmark/test_unary_pointwise_perf.py
+++ benchmark/test_unary_pointwise_perf.py
@@ -51,6 +51,7 @@ forward_operations = [
     ("rsqrt", torch.rsqrt, FLOAT_DTYPES),
     ("logical_not", torch.logical_not, INT_DTYPES + BOOL_DTYPES),
     ("log", torch.log, FLOAT_DTYPES),
+    ("log10", torch.log10, FLOAT_DTYPES),
     # ("triu", torch.triu, FLOAT_DTYPES),  # do not support 1d shapes
     # Dropout
     ("dropout", torch.nn.Dropout(p=0.5), FLOAT_DTYPES),
@@ -107,6 +108,7 @@ forward_inplace_operations = [
     ("erf_", torch.erf_, FLOAT_DTYPES),
     ("exp_", torch.exp_, FLOAT_DTYPES),
     ("exp2_", torch.exp2_, FLOAT_DTYPES),
+    ("log10_", torch.log10_, FLOAT_DTYPES),
     ("neg_", torch.neg_, FLOAT_DTYPES),
     ("reciprocal_", torch.reciprocal_, FLOAT_DTYPES),
     ("sqrt_", torch.sqrt_, FLOAT_DTYPES),
